b8dcba2a70bf6d2b0dc4392cc04767c3a047238e
Giuseppe Ciullo <web@progetty.it>
2019-09-23 18:12:06 +0200

    :zap:  create the image finder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const findImagesFrom = element => {	
-	
+    const elements = element.querySelectorAll("*");
+	const images = [...elements].map(el => {
+        const tagName = el.tagName.toLowerCase();
+		if(tagName.toLowerCase() === 'img' || tagName.toLowerCase() === 'image'){
+			return el.src;
+		} else if(el.style.backgroundImage != ""){
+			return el.style.backgroundImage.replace(/url\(/g,'').replace(/\)/,'').replace(/"/g,'');
+		}
+        return "";            
+	});
+	return images.filter(url => url!=="");
 }
 
 export default findImagesFrom;
